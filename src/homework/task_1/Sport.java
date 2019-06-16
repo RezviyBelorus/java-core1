bbd310c67a2364553b0dc60518ecd6ea22f44e99
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-16 18:42:14 +0300

    Update Sport.java

--- src/homework/task_1/Sport.java
+++ src/homework/task_1/Sport.java
@@ -3,7 +3,9 @@ package homework.task_1;
 public class Sport {
     public static void main(String [] args){
         float dayFirst=10;
+        // float distanceSum=0;
         float distanceSum=10;
+        // а в цикле сначала прибавь дистанцию к общей сумме, а затем умнож на 1.1
         for(int i=1; i<7;i++){
             dayFirst*=1.1;
             distanceSum+=dayFirst;
